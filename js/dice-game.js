dc1923f0f89d34161a3597ff1d1541968c30cdbd
Sergei Chaparin <chaparin@gmail.com>
2022-08-18 22:51:37 -0400

    Added extra features to the Dice Game

--- js/dice-game.js
+++ js/dice-game.js
@@ -2,10 +2,15 @@ function rollDice() {
     let goldCoins = 0;
     for (let i = 0; i < 10; i++) {
         const roll = Math.floor(Math.random() * 6) + 1;
-        alert('You rolled a ' + roll + '.');
+        alert('You rolled a ' + roll + '. Your running total of gold coins is ' + goldCoins + '!');
         if (roll === 1) {
             alert('Game over, no more rolls!');
             break;
+        }
+        if ((roll === 4) && (goldCoins > 0)) {
+            alert('You lost one gold coin, because you rolled a 4!');
+            goldCoins = goldCoins - 1;
+            continue;
         }
         if (roll < 5) {
             continue;
